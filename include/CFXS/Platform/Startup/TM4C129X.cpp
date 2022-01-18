d1d314e830f5983050f2a43c761994ce2bba5499
RVeips <rihardsveips@gmail.com>
2022-01-19 00:59:03 +0200

    cpu stuff

--- include/CFXS/Platform/Startup/TM4C129X.cpp
+++ include/CFXS/Platform/Startup/TM4C129X.cpp
@@ -1,30 +1,29 @@
 // ---------------------------------------------------------------------
 // CFXS Framework Platform Module <https://github.com/CFXS/CFXS-Platform>
 // Copyright (C) 2022 | CFXS / Rihards Veips
-// 
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>
 // ---------------------------------------------------------------------
 // [CFXS] //
-#if defined(CFXS_DEVICE_CLASS_TM4C129X)
 #include <type_traits>
 #include <driverlib_includes.hpp>
 #include <CFXS/Base/Cortex_M/VectorTable_TM4C129X.hpp>
-#include <CFXS/Base/CPU.hpp>
+#include <CFXS/Platform/CPU.hpp>
 
 // Empty handler for Tiva lib ASSERT
 extern "C" __weak void __error__(char *pcFilename, uint32_t ui32Line) {
-    __asm__("bkpt");
+    asm volatile("bkpt");
 }
 
 /////////////////////////////////////////////////////////////
@@ -53,19 +52,26 @@ __weak __used void __cfxs_entry_point() {
 // Default Handlers
 
 __interrupt void __isr_NMI(void) {
-    __asm__("bkpt");
+    asm volatile("bkpt");
 }
 
 __interrupt void __isr_HardFault(void) {
-    __asm__("bkpt");
+    asm volatile("bkpt");
 }
 
 __interrupt void __isr_Default(void) {
-    __asm__("bkpt");
+    asm volatile("bkpt");
 }
 
 __interrupt __noreturn __used void __Reset() {
     CFXS::CPU::DisableInterrupts();
+
+    // Disable all configurable interrupts
+    HWREG(NVIC_DIS0) = 0xFFFFFFFF;
+    HWREG(NVIC_DIS1) = 0xFFFFFFFF;
+    HWREG(NVIC_DIS2) = 0xFFFFFFFF;
+    HWREG(NVIC_DIS3) = 0xFFFFFFFF;
+    HWREG(NVIC_DIS4) = 0xFFFFFFFF;
 
     // Enable FPU
     HWREG(NVIC_CPAC) = ((HWREG(NVIC_CPAC) & ~(NVIC_CPAC_CP10_M | NVIC_CPAC_CP11_M)) | NVIC_CPAC_CP10_FULL | NVIC_CPAC_CP11_FULL);
@@ -80,14 +86,14 @@ __interrupt __noreturn __used void __Reset() {
     }
 
     // zero init
-    __asm__("    ldr     r0, =__BSS_START__ \n"
-            "    ldr     r1, =__BSS_END__   \n"
-            "    mov     r2, #0             \n"
-            "zeroinit_loop:                 \n"
-            "        cmp     r0, r1         \n"
-            "        it      lt             \n"
-            "        strlt   r2, [r0], #4   \n"
-            "        blt     zeroinit_loop    ");
+    asm volatile("    ldr     r0, =__BSS_START__ \n"
+                 "    ldr     r1, =__BSS_END__   \n"
+                 "    mov     r2, #0             \n"
+                 "zeroinit_loop:                 \n"
+                 "        cmp     r0, r1         \n"
+                 "        it      lt             \n"
+                 "        strlt   r2, [r0], #4   \n"
+                 "        blt     zeroinit_loop    ");
 
     // ctor init
     for (auto i = 0; i < __PREINIT_ARRAY_END__ - __PREINIT_ARRAY_START__; i++) {
@@ -100,11 +106,9 @@ __interrupt __noreturn __used void __Reset() {
 
     __cfxs_entry_point();
 
-    __asm__("bkpt");
+    asm volatile("bkpt");
 
-    HWREG(NVIC_APINT) = NVIC_APINT_VECTKEY | NVIC_APINT_SYSRESETREQ;
-    while (1 < 2) {
-    }
+    CFXS::CPU::Reset();
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////
@@ -117,4 +121,3 @@ __vector_table __used const CFXS::Cortex_M::VectorTable_TM4C129X<&__STACK_BASE__
     return vt;
 }
 ();
-#endif
---
+++ include/CFXS/Platform/_CPU/TM4C.hpp
@@ -0,0 +1,52 @@
+// ---------------------------------------------------------------------
+// CFXS Framework Platform Module <https://github.com/CFXS/CFXS-Platform>
+// Copyright (C) 2022 | CFXS / Rihards Veips
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>
+// ---------------------------------------------------------------------
+// [CFXS] //
+#include <inc/hw_nvic.h>
+#include <inc/hw_types.h>
+namespace CFXS::CPU {
+
+    static constexpr size_t CLOCK_FREQUENCY = CFXS_CPU_CLOCK_FREQUENCY;
+    static constexpr size_t CYCLES_PER_MS   = CLOCK_FREQUENCY / 1000;
+    static constexpr size_t CYCLES_PER_USEC = CLOCK_FREQUENCY / 1000000;
+
+    /// Reset CPU
+    static inline __noreturn void Reset() {
+        HWREG(NVIC_APINT) = NVIC_APINT_VECTKEY | NVIC_APINT_SYSRESETREQ;
+        while (1 < 2) {
+        }
+    }
+
+    /// Enable global interrupts
+    static inline void EnableInterrupts() {
+        asm volatile("cpsie i" : : : "memory");
+    }
+
+    /// Disable global interrupts
+    static inline void DisableInterrupts() {
+        asm volatile("cpsid i" : : : "memory");
+    }
+
+    /// Are global interrupts enabled
+    /// \returns true if global interrupts are enabled
+    static inline bool AreInterruptsEnabled() {
+        uint32_t primaskVal;
+        asm volatile("mrs %0, primask" : "=r"(primaskVal));
+        return primaskVal;
+    }
+
+} // namespace CFXS::CPU
