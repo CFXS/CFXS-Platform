a8ac25010edc0781299d6fa247bd96f03bf55798
RVeips <rihardsveips@gmail.com>
2022-02-02 01:27:20 +0200

    defs

--- include/CFXS/Platform/Types/Cortex_M/MPU.hpp
+++ include/CFXS/Platform/Types/Cortex_M/MPU.hpp
@@ -29,10 +29,37 @@ namespace CFXS::CPU::Types {
     };
 
     /// MPU Region Base Address Register
-    struct Reg_MPU_RBAR {};
+    struct Reg_MPU_RBAR {
+        uint32_t ADDR : 27; // Variable size based on RASR size
+        uint8_t VALID : 1;  // Reads as 0 (write 0 = REGION field ignored, RNR used; write 1 = REGION field used, RNR ignored)
+        uint8_t REGION : 4; // MPU region (reads current region specified by RNR)
+
+        inline void SetAddress(uint32_t regionSizeLog2, uint32_t addr) {
+            ADDR = addr << (regionSizeLog2 - 4);
+        }
+
+        inline void SetAddress(uint32_t regionSizeLog2, uint32_t addr, uint8_t region) {
+            REGION = region;
+            ADDR   = addr << (regionSizeLog2 - 4);
+        }
+    };
 
     /// MPU Region Attribute and Size Register
-    struct Reg_MPU_RASR {};
+    struct Reg_MPU_RASR {
+        uint8_t ENABLE : 1;     // Region enable/disable
+        uint8_t SIZE : 5;       // Size of this region
+        uint8_t _reserved0 : 2; // Reserved
+        uint8_t SRD : 8;        // Subregion disable bits
+        uint8_t B : 1;
+        uint8_t C : 1;
+        uint8_t S : 1; // Shareable
+        uint8_t TEX : 3;
+        uint8_t _reserved1 : 2; // Reserved
+        uint8_t AP : 3;         // Access permissions
+        uint8_t _reserved2 : 1; // Reserved
+        uint8_t XN : 1;         // Instruction access enable/disable
+        uint8_t _reserved3 : 3; // Reserved
+    };
 
 #pragma pack()
 
