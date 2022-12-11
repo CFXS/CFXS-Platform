9ebefcb26a65fdbcf89c431d5885402df275dcf5
RVeips <rihardsveips@gmail.com>
2022-12-11 03:34:47 +0200

    task delay offset

--- include/CFXS/Platform/Task.hpp
+++ include/CFXS/Platform/Task.hpp
@@ -1,17 +1,17 @@
 // ---------------------------------------------------------------------
 // CFXS Framework Platform Module <https://github.com/CFXS/CFXS-Platform>
 // Copyright (C) 2022 | CFXS / Rihards Veips
-// 
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>
 // ---------------------------------------------------------------------
@@ -100,6 +100,10 @@ namespace CFXS {
             if (!m_ProcessTime)
                 m_ProcessTime = CFXS::Time::ms + m_Period;
             m_Enabled = state;
+        }
+
+        constexpr void Delay(CFXS::Time_t delay_ms) {
+            m_ProcessTime += delay_ms;
         }
 
         constexpr void Start() {
