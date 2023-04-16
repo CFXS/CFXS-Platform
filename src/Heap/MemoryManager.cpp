e33ceb68c4b7df02781739b04103492d474c4219
RVeips <rihardsv@na.lv>
2023-04-16 04:36:28 +0300

    tests
    Signed-off-by: RVeips <rihardsv@na.lv>

--- src/Heap/MemoryManager.cpp
+++ src/Heap/MemoryManager.cpp
@@ -1,17 +1,17 @@
 // ---------------------------------------------------------------------
 // CFXS Framework Platform Module <https://github.com/CFXS/CFXS-Platform>
 // Copyright (C) 2022 | CFXS / Rihards Veips
-// 
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>
 // ---------------------------------------------------------------------
@@ -54,6 +54,18 @@ void operator delete(void* p, size_t s) {
 }
 
 void* operator new[](size_t size, const char* name, int flags, unsigned debugFlags, const char* file, int line) {
+    return new uint8_t[size];
+}
+
+// TODO: EASTL TEMP
+void* operator new[](size_t size,
+                     size_t alignment,
+                     size_t alignmentOffset,
+                     const char* pName,
+                     int flags,
+                     unsigned debugFlags,
+                     const char* file,
+                     int line) {
     return new uint8_t[size];
 }
 
